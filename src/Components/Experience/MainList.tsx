ad86bceab58ad845bfd10e41a1e673613b500e7f
Srishti Majumder <srishtimajumder2002@gmail.com>
2024-03-27 02:00:26 +0530

    dark mode updated

--- src/Components/Experience/MainList.tsx
+++ src/Components/Experience/MainList.tsx
@@ -9,13 +9,13 @@ const MainList = () => {
         ExperienceData.map((item) => (
           <li className="mb-10 ms-4" key={item.id}>
             <div className="absolute w-3 h-3 bg-green-400 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-green-700"></div>
-            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-300">
               {item.date}
             </time>
             <h2 className="text-lg font-bold text-gray-900 dark:text-white">
               {item.Company_name}
             </h2>
-            <p className="mb-2 font-normal text-teal-600 dark:text-slate-950">
+            <p className="mb-2 font-normal text-teal-600 dark:text-teal-500">
               {item.Role}
             </p>
             <SkillUsed skills={item.skills} />
